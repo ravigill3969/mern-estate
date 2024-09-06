f0408a717bc8f26ac342f3f0ae98544a92a19e41
RavisherSinghGill <gill1122ravi@gmail.com>
2024-09-06 06:03:56 -0400

    create update lsiting api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -23,6 +24,32 @@ export const deleteListing = async (req, res, next) => {
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json("Listing deleted successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+
+  if (listing.userRef != req.user.id) {
+    return next(errorHandler(401, "You can only update your own listings"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
