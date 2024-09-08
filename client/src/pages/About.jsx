220d759b10f64c41abd2b7a65891cb3129f9a06a
RavisherSinghGill <gill1122ravi@gmail.com>
2024-09-08 14:27:26 -0400

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,33 @@
 export default function About() {
-    return (
-      <div>About</div>
-    )
-  }
+  return (
+    <div className="py-20 px-4 max-w -6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800 ">
+        About Ravi Estate
+      </h1>
+      <p className="mb-4 text-slate-700 ">
+        Welcome to RaviEstate, your trusted partner in finding the perfect place
+        to call home. Founded by Ravi, our mission is to simplify the real
+        estate experience by offering a comprehensive platform where buyers,
+        sellers, and renters can connect seamlessly. With a deep understanding
+        of the housing market and a commitment to exceptional service, we strive
+        to provide personalized solutions that meet your unique needs.
+      </p>
+      <p className="mb-4 text-slate-700 ">
+        At RaviEstate, we believe that finding a home should be an exciting and
+        rewarding journey. Our team of experienced professionals is dedicated to
+        helping you navigate the real estate landscape with confidence. Whether
+        you&#39;re looking for your dream home, an investment property, or
+        simply exploring your options, we&#39;re here to guide you every step of
+        the way.
+      </p>
+      <p className="mb-4 text-slate-700 ">
+        Our extensive property listings and user-friendly interface ensure that
+        you have all the tools you need to make informed decisions. From
+        detailed property descriptions and high-quality images to up-to-date
+        market insights, RaviEstate offers a transparent and efficient way to
+        explore the real estate market. Join us today and discover how
+        RaviEstate can make your real estate journey smooth and successful.
+      </p>
+    </div>
+  );
+}
